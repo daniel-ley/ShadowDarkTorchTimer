fc40cf946a58c907da544e61110806cdf7d2004e
@daniel.ley <daniel.ley@otainsight.com>
2023-04-21 19:07:32 +0100

    Add button to light and relight torch

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-let torchDuration = 60;  //minutes
-var appStatus = "running";
+let torchDuration = 1;  //minutes
+let torchStatus = "unlit";
 let torchImage = document.getElementById("torchImage");
 let lightButton = document.getElementById("lightButton");
 let clock = document.getElementById('remaining_time');
 
-
 clock.innerHTML = torchDuration + " : " + "00";
-lightButton.addEventListener("click", reset);
-torchImage.src = "img/Torch_1.png";
+lightButton.addEventListener("click", lightTorch);
+torchImage.src = "img/Torch_0.png";
 
-startCountdown();
 
-function startCountdown() {
-  // if (appStatus == "stopped") {
-  //   return
-  // }
+function torchBurningLoop() {
   let presentTime = document.getElementById('remaining_time').innerHTML;
-  let timeArray = presentTime.split(/[ : ]+/);
+  let timeArray = presentTime.split(" : ");
   let minute = timeArray[0];
   let second = checkSecond((timeArray[1] - 1));
 
@@ -33,8 +28,8 @@ function startCountdown() {
   document.getElementById('remaining_time').innerHTML =
     minute + " : " + second;
 
-  setTimeout(startCountdown, 1000);
-  
+  setTimeout(torchBurningLoop, 1000);
+
 }
 
 function checkSecond(sec) {
@@ -48,15 +43,19 @@ function checkSecond(sec) {
 }
 
 function manageTorch(min, sec) {
-  if (min == 45 && sec == 0) {
+  if (min == 0 && sec == 59) {
+    torchImage.src = "img/Torch_1.png";
+  }
+
+  if (min == 0 && sec ==45) {
     torchImage.src = "img/Torch_2.png";
   };
 
-  if (min == 30 && sec == 0) {
+  if (min == 0 && sec == 30) {
     torchImage.src = "img/Torch_3.png";
   };
 
-  if (min == 15 && sec == 0) {
+  if (min == 0 && sec == 15) {
     torchImage.src = "img/Torch_4.png";
   };
 
@@ -70,10 +69,14 @@ function manageTorch(min, sec) {
 }
 
 
-function reset(){
-  // appStatus = "stopped";
+function lightTorch(){
+  if (torchStatus == "unlit" || clock.innerHTML == "00 : 00") {
+  clock.innerHTML = torchDuration + " : " + "00";
+  torchStatus = "burning";
   torchImage.src = "img/Torch_1.png";
-  document.getElementById('remaining_time').innerHTML =
-    torchDuration + " : " + "00";
+  torchBurningLoop();
+  } else {
+  clock.innerHTML = torchDuration + " : " + "00";
+  torchImage.src = "img/Torch_1.png";
+  }
 }
-
